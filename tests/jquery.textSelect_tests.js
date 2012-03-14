4def7757dd3746c40529ee61db97f9da3a87b4da
James M. Greene <james.m.greene@gmail.com>
2012-03-14 01:18:26 -0500

    Updated shell tests and added more test modules

--- tests/jquery.textSelect_tests.js
+++ tests/jquery.textSelect_tests.js
@@ -1,15 +1,20 @@
-module("Test jquery.textSelect via the jQuery plugin function rather than via a direct binding mechanism");
-
-test("$.textSelect with a handler will hookup event", function() {
-	ok(false, "Not implemented yet!");
-});
-
-test("$.textSelect without a handler will trigger event", function() {
-	ok(false, "Not implemented yet!");
-});
-
-
 module("Test jquery.textSelect via the event system's main binding mechanism: on/off");
+
+test("Add tests here", function() {
+	ok(false, "Not implemented yet!");
+});
+
+
+module("Test jquery.textSelect via the jQuery plugin function rather than via a binding mechanism");
+
+test("Add tests here", function() {
+	ok(false, "Not implemented yet!");
+});
+// Test: $.textSelect with a handler will hookup event
+// Test: $.textSelect without a handler will trigger event
+
+
+module("Test overriding the jquery.textSelect default behavior implementations");
 
 test("Add tests here", function() {
 	ok(false, "Not implemented yet!");
