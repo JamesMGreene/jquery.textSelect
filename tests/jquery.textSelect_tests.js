f6fb3222a835e3870f26cfe942b15c1f25e457a3
James M. Greene <james.m.greene@gmail.com>
2012-03-14 01:03:42 -0500

    Adding the QUnit tests boilerplate

---
+++ tests/jquery.textSelect_tests.js
@@ -0,0 +1,37 @@
+module("Test jquery.textSelect via the jQuery plugin function rather than via a direct binding mechanism");
+
+test("$.textSelect with a handler will hookup event", function() {
+	ok(false, "Not implemented yet!");
+});
+
+test("$.textSelect without a handler will trigger event", function() {
+	ok(false, "Not implemented yet!");
+});
+
+
+module("Test jquery.textSelect via the event system's main binding mechanism: on/off");
+
+test("Add tests here", function() {
+	ok(false, "Not implemented yet!");
+});
+
+
+module("Test jquery.textSelect via the event system's deprecated binding mechanism: bind/unbind");
+
+test("Add tests here", function() {
+	ok(false, "Not implemented yet!");
+});
+
+
+module("Test jquery.textSelect via the event system's deprecated binding mechanism: live/die");
+
+test("Add tests here", function() {
+	ok(false, "Not implemented yet!");
+});
+
+
+module("Test jquery.textSelect via the event system's deprecated binding mechanism: delegate/undelegate");
+
+test("Add tests here", function() {
+	ok(false, "Not implemented yet!");
+});
